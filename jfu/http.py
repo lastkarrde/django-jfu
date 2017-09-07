73b27633bd54a931e0d66aba5d07f4338dbdaedd
lmortimer <logan.mortimer@pacificedge.co.nz>
2017-09-08 10:53:13 +1200

    pylint code clean up

--- jfu/http.py
+++ jfu/http.py
@@ -1,15 +1,17 @@
+"""JFU Response objects"""
+
 import json
 from django.http import HttpResponse
 
 
-def upload_receive( request ):
+def upload_receive(request):
     """
     Returns the file(s) uploaded by the user.
     """
     return request.FILES['files[]'] if request.FILES else None
 
 
-class JFUResponse( HttpResponse ):
+class JFUResponse(HttpResponse):
     """
     Returns an HTTP response with its data encoded in JSON format.
 
@@ -23,36 +25,36 @@ class JFUResponse( HttpResponse ):
     declare a JSON MIME-type.
     """
 
-    def __init__( self, request, data = True, *args, **kwargs ):
-        data   = json.dumps( data )
-        j      = "application/json"
+    def __init__(self, request, data=True, *args, **kwargs):
+        data = json.dumps(data)
+        j = "application/json"
         accept = request.META.get('HTTP_ACCEPT')
 
         mime = j if accept and j in accept else 'text/plain'
 
-        super( JFUResponse, self ).__init__( data, mime, *args, **kwargs )
-    
+        super(JFUResponse, self).__init__(data, mime, *args, **kwargs)
 
-class UploadResponse( JFUResponse ):
+
+class UploadResponse(JFUResponse):
     """
     Takes a dictionary containing the required jQuery File Upload fileupload
     response data and returns a JFUResponse.
 
     The dictionary may take the following form:
     {
-       'name': 'file name', 
+       'name': 'file name',
        'size': 12345,
-       'url' : 'file url', 
+       'url' : 'file url',
        'thumbnailUrl': 'thumbnail url',
        'deleteUrl'   : 'delete url',
-       'deleteType'  : 'delete type', 
+       'deleteType'  : 'delete type',
     }
 
     Refer to the jQuery File Upload documentation for acceptable response data.
     [https://github.com/blueimp/jQuery-File-Upload/wiki/Setup]
     """
 
-    def __init__( self, request, file_dict, *args, **kwargs ):
-        files = file_dict if isinstance( file_dict, list ) else [ file_dict ]
-        data  = { 'files' : files }
-        super( UploadResponse, self ).__init__( request, data, *args, **kwargs )
+    def __init__(self, request, file_dict, *args, **kwargs):
+        files = file_dict if isinstance(file_dict, list) else [file_dict]
+        data = {'files': files}
+        super(UploadResponse, self).__init__(request, data, *args, **kwargs)
---
+++ jfu/templatetags/__init__.py
@@ -0,0 +1,1 @@
+"""Available template tags"""
